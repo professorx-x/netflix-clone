52e370aa8e8b6cabb2179d027c53dc29a0622e9b
Leonardo Silva <61755496+professorx-x@users.noreply.github.com>
2021-02-25 20:23:45 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,35 @@
+function playvideo(num)
+{
+	/*
+	num = num -1;
+
+	var item = document.querySelectorAll('.item');
+	var boxFilme = document.querySelectorAll('.box-filme');
+	var video = document.querySelectorAll('video');
+
+	//boxFilme[num].style.opacity = 0;
+
+	if(innerWidth >= 1000)
+	{
+		item[num].style.width = "200%";
+	}
+
+
+	setTimeout(function()
+	{
+		boxFilme[num].style.display = "none";		
+		video[num].style.display = "block";
+		
+		
+	}, 500)
+
+	setTimeout(function()
+	{
+		//video[num].style.opacity = 1;
+		video[num].play();
+	}, 800)
+	*/	
+
+	//alert(boxFilme.length);
+	//alert(boxFilme[num]);
+}
